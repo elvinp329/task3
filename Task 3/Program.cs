ea6f92013a0918310529ddf21dba98c6cf3469dd
Elvinp329 <elvinkj@code.edu.az>
2022-12-28 02:29:58 +0400

    task3

---
+++ Task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 16538;
+            int a = 4 ; int b = 2;
+            Console.WriteLine($"{a}{i}{b}");
+
+
+
+            
+        }
+    }
+}
